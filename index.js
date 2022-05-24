89b0cde8e6eed6769039848a4f3c2ccec191da01
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-24 22:11:51 +0600

    purchase id Api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -19,7 +19,14 @@ async function run (){
             const cursor= itemsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
-        })
+        });
+
+        app.get('/purchase/:id',async(req,res)=>{
+            const id=req.params.id ;
+            const query={_id:ObjectId(id)};
+            const inventory = await itemsCollection.findOne(query);
+            res.send(inventory);
+         });
         
 
 
