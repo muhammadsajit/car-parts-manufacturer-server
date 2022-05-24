2ca662afae315c413e81057936e8384942333bb3
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-24 21:30:43 +0600

    items api added

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.daakn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run (){
+    try{
+
+        await client.connect();
+        const itemsCollection=client.db('car_parts_manufacturer').collection('items');
+        app.get('/items',async(req,res)=>{
+            const query={};
+            const cursor= itemsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
-
+//0WgaES3xRJmjmSdW
+//car_user
 app.listen(port, () => {
     console.log(`car app listening on port ${port}`)
 })
